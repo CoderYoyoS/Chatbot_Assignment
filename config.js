174f9dbd3c15539b8c0dddc81734bbd6d5b520c9
aaron ward <aaron ward>
2017-03-30 19:25:23 +0100

    Added app secret

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAZA70jtCZAh4BABsScx7jqucxydcn16uMZCXcNT4c9NErZCRGnAFRx0ohw9RAjZAL1pxZCxNiF5eKlEdRp4xVCgVfkn0CtqOIiK6gZBfy6PXq5CNfMX3e1GUiZB2sbzFy7YRWZBhM5ZBmMx43aEZCvpxDz6wZAwXlD0qJwr1ZA1fP4PxvxjS25Tvk881',
     FB_VERIFY_TOKEN: 'secret',
-    API_AI_CLIENT_ACCESS_TOKEN: '5c09189e70614b58a8ad579c364bbf3b ',
-    FB_APP_SECRET: 'secret',
+    API_AI_CLIENT_ACCESS_TOKEN: '5c09189e70614b58a8ad579c364bbf3b',
+    FB_APP_SECRET: '8e3c62def3af61939a9c4ba4628517f3',
     SERVER_URL: "https://itbchatbot.herokuapp.com/",
 };
