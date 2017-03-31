859d1604f0f1ab43a022dd892bcd4e60648c7422
aaron ward <aaron ward>
2017-03-31 14:23:06 +0100

    Added API urls to config.js

--- config.js
+++ config.js
@@ -1,7 +1,20 @@
+
+/**
+ * These are used by the app of authentication.
+ * 
+ * DO NOT CHANGE THESE .. Only if you generate a new token
+ * 
+ * Server URL's are the used for accessing our API's
+ */
 module.exports = {
     FB_PAGE_TOKEN: 'EAAZA70jtCZAh4BAKdB2uUEaubMaIZC7JzhhmkcAuAxsmIefecSNvIjeUdExJnYrAqNOtlkbnusnhgRXKUklmqYBbaj1JLdE1sPu0knPcvzZCaqyzwfBWv6lvrL0IHtYcB3LeWmq8phu4ZAtt5xqd5BjyGt673NxAnE18Y0acShbE3D3TZA6sZCl',
     FB_VERIFY_TOKEN: 'secret',
-    API_AI_CLIENT_ACCESS_TOKEN: '5c09189e70614b58a8ad579c364bbf3b',
     FB_APP_SECRET: '8e3c62def3af61939a9c4ba4628517f3',
+
+    API_AI_CLIENT_ACCESS_TOKEN: '5c09189e70614b58a8ad579c364bbf3b',
+    
     SERVER_URL: "https://itbchatbot.herokuapp.com/",
+    LIBRARY_SERVER_URL : "https://daireapi.herokuapp.com/", 
+    BUS_SERVER_URL : "https://aaronapi.herokuapp.com/", 
+    GYM_SERVER_URL : "https://brianapi.herokuapp.com/"
 };
