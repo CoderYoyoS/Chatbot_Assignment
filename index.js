d9ca769617fc165529fec19022a61c42e8903f9a
aaron ward <aaron ward>
2017-04-11 14:44:26 +0100

    test message data

--- index.js
+++ index.js
@@ -788,27 +788,51 @@ function receivedPostback(event) {
 
 			console.log('\x1b[36m', "----- I GOT TO THE GET STARTED PAYLOAD --------", '\x1b[0m');
 
+			// var messageData = {
+			// text: "Hi, I am the ITB Chatbot🤖 Im here to help you through college and make your college life easier😃So lets get started 😏",
+			// quick_replies :[
+			// 		{
+			// 			content_type :"text",
+			// 			title : "What can you do?",
+			// 			payload : "What can you do?"
+			// 		},
+			// 		{
+			// 			content_type :"text",
+			// 			title : "Who made you?",
+			// 			payload : "Who made you?"
+			// 		},
+			// 		{
+			// 			content_type :"text",
+			// 			title : "Privacy policy",
+			// 			payload : "Privacy policy"
+			// 		}
+			// 	]
+			// }
+
 			var messageData = {
-			text: "Hi, I am the ITB Chatbot🤖 Im here to help you through college and make your college life easier😃So lets get started 😏",
-			quick_replies :[
-					{
-						content_type :"text",
-						title : "What can you do?",
-						payload : "What can you do?"
-					},
-					{
-						content_type :"text",
-						title : "Who made you?",
-						payload : "Who made you?"
-					},
-					{
-						content_type :"text",
-						title : "Privacy policy",
-						payload : "Privacy policy"
-					}
-				]
-			}
-			sendTextMessage(senderID, "Hello test");
+				message: {
+					text:"Hi, I am the ITB Chatbot🤖 Im here to help you through college and make your college life easier😃So lets get started 😏",
+					quick_replies:[
+						{
+							content_type :"text",
+							title : "What can you do?",
+							payload : "What can you do?"
+						},
+						{
+							content_type :"text",
+							title : "Who made you?",
+							payload : "Who made you?"
+						},
+						{
+							content_type :"text",
+							title : "Privacy policy",
+							payload : "Privacy policy"
+						}
+					]
+				}
+			};
+
+			sendTextMessage(senderID,messageData );
 			// callSendAPI(messageData);
 				break;
 		default:
