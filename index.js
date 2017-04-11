342e2d7d9b96962981822115ef63da5f33b84a60
aaron ward <aaron ward>
2017-04-11 14:33:52 +0100

    added test console.log

--- index.js
+++ index.js
@@ -787,31 +787,36 @@ function receivedPostback(event) {
 		case 'GET_STARTED' :
 
 			console.log('\x1b[36m', "----- I GOT TO THE GET STARTED PAYLOAD --------", '\x1b[0m');
+				console.log('\x1b[36m', "----- " +recipientID +" --------", '\x1b[0m');
+
+
 
 			var messageData = {
 				recipient: { 
 					id: recipientID
 				},
-				message : {
-				text: "Hi, I am the ITB Chatbot🤖 Im here to help you through college and make your college life easier😃So lets get started 😏",
-				quick_replies :[
-						{
-							content_type :"text",
-							title : "What can you do?",
-							payload : "What can you do?"
-						},
-						{
-							content_type :"text",
-							title : "Who made you?",
-							payload : "Who made you?"
-						},
-						{
-							content_type :"text",
-							title : "Privacy policy",
-							payload : "Privacy policy"
-						}
-					]
-				}
+				messages : [
+					{
+					text: "Hi, I am the ITB Chatbot🤖 Im here to help you through college and make your college life easier😃So lets get started 😏",
+					quick_replies :[
+							{
+								content_type :"text",
+								title : "What can you do?",
+								payload : "What can you do?"
+							},
+							{
+								content_type :"text",
+								title : "Who made you?",
+								payload : "Who made you?"
+							},
+							{
+								content_type :"text",
+								title : "Privacy policy",
+								payload : "Privacy policy"
+							}
+						]
+					}
+				]
 			}
 			callSendAPI(messageData);
 				break;
