b1ec6e38b254029881b4ec0f1db4de59323a34f4
aaron ward <aaron ward>
2017-04-12 18:40:31 +0100

    Updated handleApiAiResponse

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ function handleCardMessages(messages, sender) {
  * @param {*} response 
  */
 function handleApiAiResponse(sender, response) {
-	let responseText = response.result.fulfillment.speech;
+let responseText = response.result.fulfillment.speech;
 	let responseData = response.result.fulfillment.data;
 	let messages = response.result.fulfillment.messages;
 	let action = response.result.action;
@@ -379,35 +379,23 @@ function handleApiAiResponse(sender, response) {
 
 	sendTypingOff(sender);
 
-
-	console.log(responseText, "\n", messages, "\n", action, "\n", parameters, "\n", contexts);
-
-
-
-	//isDefined() is a function used to check that the variables have a value
 	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
-		
 		let timeoutInterval = 1100;
 		let previousType ;
 		let cardTypes = [];
 		let timeout = 0;
-
 		for (var i = 0; i < messages.length; i++) {
+
 			if ( previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
 
 				timeout = (i - 1) * timeoutInterval;
-
 				setTimeout(handleCardMessages.bind(null, cardTypes, sender), timeout);
-				
 				cardTypes = [];
 				timeout = i * timeoutInterval;
-				
 				setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
-			} 
-			else if (messages[i].type == 1) {
+			} else if ( messages[i].type == 1 ) {
 				cardTypes.push(messages[i]);
-			} 
-			else {
+			} else {
 				timeout = i * timeoutInterval;
 				setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
 			}
@@ -415,6 +403,21 @@ function handleApiAiResponse(sender, response) {
 			previousType = messages[i].type;
 
 		}
+	} else if (responseText == '' && !isDefined(action)) {
+		//api ai could not evaluate input.
+		console.log('Unknown query' + response.result.resolvedQuery);
+		sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
+	} else if (isDefined(action)) {
+		handleApiAiAction(sender, action, responseText, contexts, parameters);
+	} else if (isDefined(responseData) && isDefined(responseData.facebook)) {
+		try {
+			console.log('Response as formatted message' + responseData.facebook);
+			sendTextMessage(sender, responseData.facebook);
+		} catch (err) {
+			sendTextMessage(sender, err.message);
+		}
+	} else if (isDefined(responseText)) {
+		sendTextMessage(sender, responseText);
 	} 
 	//If the response does not have a value
 	else if (responseText == '' && !isDefined(action)) {
