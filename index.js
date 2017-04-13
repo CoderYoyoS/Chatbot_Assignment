dd0b5741efac04f160f00c8158c602b187420728
aaron ward <aaron ward>
2017-04-13 16:49:54 +0100

    took out card handle

--- index.js
+++ index.js
@@ -332,50 +332,6 @@ function handleMessage(message, sender) {
 
 
 /**
- * This is used for sending cards to
- * facebook user
- * @param {*} messages 
- * @param {*} sender 
- */
-// function handleCardMessages(messages, sender) {
-
-// 	let elements = [];
-// 	for (var i = 0; i < messages.length; i++) {
-// 		let message = messages[i];
-// 		let buttons = [];
-// 		for (var b = 0; b < message.buttons.length; b++) {
-// 			let isLink = (message.buttons[b].postback.substring(0, 4) === 'http');
-// 			let button;
-// 			if (isLink) {
-// 				button = {
-// 					"type": "web_url",
-// 					"title": message.buttons[b].text,
-// 					"url": message.buttons[b].postback
-// 				}
-// 			} else {
-// 				button = {
-// 					"type": "postback",
-// 					"title": message.buttons[b].text,
-// 					"payload": message.buttons[b].postback
-// 				}
-// 			}
-// 			buttons.push(button);
-// 		}
-
-
-// 		let element = {
-// 			"title": message.title,
-// 			"image_url":message.imageUrl,
-// 			"subtitle": message.subtitle,
-// 			"buttons": buttons
-// 		};
-// 		elements.push(element);
-// 	}
-// 	sendGenericMessage(sender, elements);
-// }
-
-
-/**
  * Handles the responsed from API.ai
  * @param {*} sender 
  * @param {*} response 
