63b5a315933d9f7d6f5cdb22c8b6c0f4044bb1e6
aaron ward <aaron ward>
2017-04-26 18:57:00 +0100

    cleaned up code

--- index.js
+++ index.js
@@ -70,18 +70,9 @@ app.get('/webhook/', function (req, res) {
 	}
 })
 
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page. 
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
 app.post('/webhook/', function (req, res) {
 	var data = req.body;
-	// console.log(JSON.stringify(data));
 
-	// Make sure this is a page subscription
 	if (data.object == 'page') {
 		// Iterate over each entry
 		// There may be multiple if batched
